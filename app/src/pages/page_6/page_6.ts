2be8ab26dce2af2c142648a9280bd423c429a922
panchenk <panchenk@email.sc.edu>
2018-01-02 00:05:12 -0500

    added date display to the first page

--- app/src/pages/page_6/page_6.ts
+++ app/src/pages/page_6/page_6.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { TestedDateService } from '../../services/testedDate';
 
 @Component({
   selector: 'page_6',
   templateUrl: 'page_6.html'
 })
 export class Page6 {
+  today: String;
 
-  constructor(public navCtrl: NavController, private iab: InAppBrowser) {
-
+  constructor(public navCtrl: NavController, private iab: InAppBrowser, public testingService: TestedDateService) 
+  {
+    this.today = testingService.getTestedDate().toISOString();
   }
 
   openWebsite(website) {
