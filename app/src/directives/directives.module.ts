17e1b59d62aa3651f88428080efe2efdd362e9a6
kostfreelance <kost.freelance@gmail.com>
2018-01-05 17:08:26 +0300

    prod mode fix

--- app/src/directives/directives.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ElasticTextareaDirective } from './elastic-textarea/elastic-textarea';
-@NgModule({
-	declarations: [ElasticTextareaDirective],
-	imports: [],
-	exports: [ElasticTextareaDirective]
-})
-export class DirectivesModule {}
